1f64d2f1998cff17a87f1741c21e0ea8418bfb46
VellStraggler <david@incwells.com>
2022-11-08 21:08:38 -0700

    ms3 try again

--- src/main/com/adventure/Main.java
+++ src/main/com/adventure/Main.java
@@ -8,12 +8,11 @@ import java.util.Scanner;
 public class Main {
 
     private static GameController controller = new GameController();
+    private static Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
 
     private static void setScanner(Scanner scanner) {
         Main.scanner = scanner;
     }
-
-    private static Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
 
     /**
      * The main function that gets run when the program starts.
